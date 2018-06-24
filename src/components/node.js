ba542650aff463acec0b3769d06ef6a1cfb5d751
Andrew Clavin <andrewclavin8@gmail.com>
2018-06-23 23:50:54 -0400

    bugfix: edit mode child inconsistency, better dragging

--- src/components/node.js
+++ src/components/node.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import './node.css'
+import {Node as RecNode} from './'
 
 class Node extends Component {
     constructor(props) {
       super(props)
-
+      this.dragger = React.createRef()
       this.state = {
           text: 'Node Name',
           isEdit: false,
@@ -36,36 +37,42 @@ class Node extends Component {
     }
 
     drag = (ev) => {
+        //the should edit for child nodes is being attached to the root node
+        ev.stopPropagation()
         this.setState({
             ...this.state,
             pos1: this.state.pos3 - ev.clientX,
             pos2: this.state.pos4 - ev.clientY,
             pos3: ev.clientX,
             pos4: ev.clientY,
-        })
-        ev.target.parentNode.style.top = (ev.target.parentNode.offsetTop - this.state.pos2) + "px"
-        ev.target.parentNode.style.left = (ev.target.parentNode.offsetLeft - this.state.pos1) + "px"
-        this.setState({
             shouldEdit: false
         })
+        this.dragger.current.style.top = (this.dragger.current.offsetTop - this.state.pos2) + "px"
+        this.dragger.current.style.left = (this.dragger.current.offsetLeft - this.state.pos1) + "px"
+
     }
 
     startDrag = (ev) => {
+        ev.stopPropagation()
         this.setState({
             ...this.state,
             pos3: ev.clientX,
             pos4: ev.clientY,
         })
-        ev.target.onmousemove = this.drag
+        //this is why the mousemove is needing to be to its parents
+        this.dragger.current.onmousemove = this.drag
+        document.onmouseup = this.stopDrag
     }
 
     stopDrag = (ev) => {
+        ev.stopPropagation()
         //TODO: stop drag and drag need to be about the mouse event anywhere, making it based on the event target isn't reliable
         //allows you to move quicker than the browser's response and get off the object but leave the event live
-        ev.target.onmousemove = null
+        this.dragger.current.onmousemove = null
     }
 
-    addNode = () => {
+    addNode = (ev) => {
+        ev.stopPropagation()
         this.setState({
             ...this.state,
             children: [...this.state.children, {}]
@@ -76,17 +83,17 @@ class Node extends Component {
   render() {
     return (
         <div className="nodeWrap">
-            <div className="dragger" onMouseDown={this.startDrag} onMouseUp={this.stopDrag}>
+            <div className="dragger" onMouseDown={this.startDrag} ref={this.dragger} >
           <div className="node" onClick={this.toggleEdit}>
               {this.state.isEdit ? (<input type="text" name="text" value={this.state.text} onChange={this.handleChange} onClick={(ev) => {ev.stopPropagation()}}/>) : (<span>{this.state.text}</span>)}
               <svg className="nodeIcon" xmlns="http://www.w3.org/2000/svg" width="100%" viewBox="0 0 32 32" aria-labelledby="title">
   	           <title id="title">Umbrella Icon</title>
                  <rect width="100%" height="50%" style={{fill:'rgb(255,255,255)',strokeWidth:'0.01em',stroke:'rgb(0,0,0)'}} />
              </svg>
+             <button onClick={this.addNode}>Add </button>
           </div>
-          <button onClick={this.addNode}>Add </button>
           {this.state.children.map(node => {
-              return <Node />
+              return <RecNode />
           })
           }
           </div>
