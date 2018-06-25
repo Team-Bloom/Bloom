27f208a67588774a83d008b74dad82b1de67e7e1
Zac Peires <zacpeires@googlemail.com>
2018-06-25 15:10:19 -0400

    feature: can get user on navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-// import firebase from 'firebase'
-import { Link } from 'react-router-dom'
+import firebase from 'firebase';
+import { Link } from 'react-router-dom';
 
 class Navbar extends Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
       projectName: '',
@@ -12,120 +12,136 @@ class Navbar extends Component {
       userName: '',
       userEmail: '',
       recipientName: '',
-      recipientEmail: ''
+      recipientEmail: '',
+      // checkedForUser: false,
+    };
 
-    }
-
-    this.saveProject = this.saveProject.bind(this)
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.showForm = this.showForm.bind(this)
-    this.hideForm = this.hideForm.bind(this)
+    this.saveProject = this.saveProject.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.showForm = this.showForm.bind(this);
+    this.hideForm = this.hideForm.bind(this);
+    this.authUser = this.authUser.bind(this);
   }
 
-
   componentDidMount() {
-      // const user = firebase.auth().currentUser
-      // if (user) {
-      //   this.setState({
-      //     userName: user.name,
-      //     userEmail: user.email
-      //   })
-      // }
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.authUser(user);
+      } else {
+        console.log('no user');
+      }
+    });
+  }
 
-
-    // console.log(user)
+  authUser(user) {
+    this.setState({
+      userName: user.displayName,
+      userEmail: user.email,
+    });
   }
 
   saveProject(event) {
+
 
   }
 
   handleChange(event) {
     this.setState({
-      [event.target.name]: event.target.value
-    })
-    console.log(this.state.recipientEmail)
+      [event.target.name]: event.target.value,
+    });
 
+    const user = firebase.auth().currentUser;
+    if (user) {
+      console.log(user);
+    }
   }
-
 
   handleSubmit(event) {
     event.preventDefault();
   }
 
-
   hideForm() {
-    const popup = document.getElementById("collab-form");
-    if (popup.classList.contains("show")) {
-      popup.classList.remove("show");
+    const popup = document.getElementById('collab-form');
+    if (popup.classList.contains('show')) {
+      popup.classList.remove('show');
     }
   }
 
   showForm() {
-  const popup = document.getElementById("collab-form");
-  if (!popup.classList.contains("show")) {
-    popup.classList.add("show");
-  }
+    const popup = document.getElementById('collab-form');
+    if (!popup.classList.contains('show')) {
+      popup.classList.add('show');
+    }
   }
 
   render() {
-    return (!this.state.userName) ? (
+    return !this.state.userName ? (
       <ul className="navbar-container">
-      <li className="dropdown">
-        <span className="dropbtn">Bloom</span>
-        <div className="dropdown-content">
-          <Link to="/user-page">
-          <span>Profile & account</span>
-          </ Link>
-          <span>Preferences</span>
-        </div>
-      </li>
-      <li className="dropdown">
-        <span className="dropbtn" onMouseOver={this.hideForm}>Projects</span>
-        <div className="dropdown-content">
-         <span>New project</span>
-          <span>Save as</span>
-          <span>Open...</span>
-          <div className="popup" onClick={this.showForm}>
-          <span>Add collaborator</span>
+        <li className="dropdown">
+          <span className="dropbtn">Bloom</span>
+          <div className="dropdown-content">
+            <Link to="/user-page">
+              <span>Profile & account</span>
+            </Link>
+            <span>Preferences</span>
+          </div>
+        </li>
+        <li className="dropdown">
+          <span className="dropbtn" onMouseOver={this.hideForm}>
+            Projects
+          </span>
+          <div className="dropdown-content">
+            <span>New project</span>
+            <span>Save as</span>
+            <span>Open...</span>
+            <div className="popup" onClick={this.showForm}>
+              <span>Add collaborator</span>
 
-          <form className="popuptext" id="collab-form" autoComplete="off">
-
-     <label htmlFor="recipientName">First and last name</label>
-      <input
-        className="recipientName"
-        type="text"
-
-        name="recipientName"
-        onChange={this.state.handleChange}
-        value={this.state.recipientName}
-      />
-      <label htmlFor="recipientEmail">Email</label>
-      <input
-        className="recipientEmail"
-        type="text"
-        name="recipientEmail"
-        onChange={this.handleChange}
-        value={this.state.recipientEmail}
-      />
-      <button type="submit" className='email-form-btn' onClick={this.handleSubmit}>
-        Share
-      </button>
-         </form>
-        </div>
-        </div>
-      </li>
-      <li className="logged-in"><span>Sign out</span></li>
-    </ul>
+              <form className="popuptext" id="collab-form" autoComplete="off">
+                <label htmlFor="recipientName">First and last name</label>
+                <input
+                  className="recipientName"
+                  type="text"
+                  name="recipientName"
+                  onChange={this.handleChange}
+                  value={this.state.recipientName}
+                />
+                <label htmlFor="recipientEmail">Email</label>
+                <input
+                  className="recipientEmail"
+                  type="text"
+                  name="recipientEmail"
+                  onChange={this.handleChange}
+                  value={this.state.recipientEmail}
+                />
+                <button
+                  type="submit"
+                  className="email-form-btn"
+                  onClick={this.handleSubmit}
+                >
+                  Share
+                </button>
+              </form>
+            </div>
+          </div>
+        </li>
+        <li className="logged-in">
+          <span>Sign out</span>
+        </li>
+      </ul>
     ) : (
       <ul className="navbar-container">
-           <Link to='/login'>
-      <li><span>Sign-in</span></li>
-      </Link>
-      <li><span>New project</span></li>
+        <Link to="/login">
+          <li>
+            <span>Sign-in</span>
+          </li>
+        </Link>
+        <li>
+          <span>New project</span>
+        </li>
       </ul>
-    )
+    );
   }
 }
 
