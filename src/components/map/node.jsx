c98a3766ea938ae9c9fa447bc9bd630b2666b503
Andrew Clavin <andrewclavin8@gmail.com>
2018-06-29 15:04:09 -0400

    refactor(node.tmpl): removing a giant this object

--- src/components/map/node.jsx
+++ src/components/map/node.jsx
@@ -80,7 +80,16 @@ class Node extends Component {
 
   render() {
     return (
-      <NodeTmpl that={this} />
+        //don't need this anymore!!!!!
+      <NodeTmpl node={this.state.node}
+        dragger={this.dragger}
+        func={(ev) => {makeDraggable(ev, this)}}
+        addNode={this.addNode}
+        handleChange={this.handleChange}
+        checkState={this.checkState}
+        deleteNode={this.deleteNode}
+        count={this.props.count}
+        />
     );
   }
 }
