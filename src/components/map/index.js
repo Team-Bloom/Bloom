89db9df82f5d9b3c037cfb7f418b180bf06cf15c
Andrew Clavin <andrewclavin8@gmail.com>
2018-06-28 23:54:49 -0400

    refactor(nodes): making dumb stateless components, nodes is a pretty messy one currently

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -1,2 +1,4 @@
 export { default as Node } from './node.jsx'
 export { default as NodeObject } from './nodeObject.jsx'
+export { default as NodeTmpl } from './node.tmpl.jsx'
+export { default as NodeObjectTmpl } from './nodeObject.tmpl.jsx'
