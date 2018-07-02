fe00ef1c73bff8b18fedf01d56cb51001d56f6a6
Zac Peires <zacpeires@googlemail.com>
2018-07-02 15:08:54 -0400

    fix: deleted edit user - not possible

--- src/components/Users/UserPage.jsx
+++ src/components/Users/UserPage.jsx
@@ -10,7 +10,6 @@ class UserPage extends Component {
     this.state = {
       userName: '',
       userEmail: '',
-      userPassword: '',
       editUserName: false
     };
 
@@ -32,58 +31,15 @@ class UserPage extends Component {
 
   }
 
-
   async handleSubmit(event) {
     event.preventDefault();
     const prevEmail = this.props.user.metadata.email
-    const projects = await this.props.user.projects
     const prevUser = this.props.user
-    const listOfCollaborators = []
 
-
-    for (let key in projects) {
-      const collaborators = projects[key].collaborators
-      const project = projects[key]
-      let owner = projects[key].owner
-
-
-      let userIndex;
-      collaborators.forEach((collaborator, index) => {
-        if (collaborator.email === owner) {
-          userIndex = +index
-        }
-      })
-
-
-      const updatedCollaborators = [...collaborators.slice(0, userIndex), {email: this.state.userEmail, name: this.state.userName}, ...collaborators.slice(userIndex +1)]
-      console.log(updatedCollaborators)
-
-
-      if (prevEmail === owner) {
-        owner = this.state.userEmail
-      }
-
-
-
-
-      await db.collection('Projects').doc(key).update({...project, 'metadata': {
-       collaborators: updatedCollaborators}, owner: owner
-      })
-
-      await db.collection('Users').doc(this.state.userEmail).update({...prevUser, 'metadata': {
-        name: this.state.userName, email: this.state.userEmail, uid: prevUser.metadata.uid
-      } })
-
-      updatedCollaborators.forEach(async collaborator => {
-          db.collection('Users').doc(collaborator.email).update({
-            ['projects.' + key]: {
-              ...projects[key], collaborators: updatedCollaborators, owner: owner
-            }
-          })
-        })
-      }
+    await db.collection('Users').doc(prevEmail).update({...prevUser, 'metadata': {
+      name: this.state.userName, email: this.state.userEmail, uid: prevUser.metadata.uid
+    } })
     }
-
 
 
   render() {
