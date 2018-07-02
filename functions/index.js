a32fe8f68dc4b7ffed587287c6c37aba5481fc5a
Kirk Sullivan <kirk_135@hotmail.com>
2018-07-02 14:58:24 -0400

    Trying a fixed Video chat

--- functions/index.js
+++ functions/index.js
@@ -16,7 +16,7 @@ app.get('*', (request, response) => {
   // containing the grant we just created
   var token = new AccessToken(ACCOUNT_SID, API_KEY_SID, API_KEY_SECRET)
   // var token = new AccessToken(functions.config().twilio.account_sid, functions.config().twilio.api_key_sid, functions.config().twilio.api_key_secret);
-
+  console.log(token)
   // Assign the generated identity to the token
   token.identity = identity;
 
