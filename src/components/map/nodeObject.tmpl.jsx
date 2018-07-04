0f2093aefc0314bd24a3ee0292a4e8bd0ceb3bef
Raghava Kamalesh <raghava.kamalesh@gmail.com>
2018-07-04 16:15:27 -0400

    added back cut/paste disabling logic

--- src/components/map/nodeObject.tmpl.jsx
+++ src/components/map/nodeObject.tmpl.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 function NodeObjectTmpl(props) {
   return (
-    <div className={`${props.pointed ? 'node pointed' : 'node'}`} onClick={props.toggleEdit}>
+    <div
+      className={`${props.pointed ? 'node pointed' : 'node'}`}
+      onClick={props.toggleEdit}
+    >
       {props.isEdit ? (
         <input
           type="text"
@@ -16,11 +19,27 @@ function NodeObjectTmpl(props) {
       ) : (
         <span>{props.text}</span>
       )}
-      <div className={`${props.isEdit ? 'functionButtons visible' : 'functionButtons'}`}>
+      <div
+        className={`${
+          props.isEdit ? 'functionButtons visible' : 'functionButtons'
+        }`}
+      >
         <button onClick={props.addNode}>Add </button>
-        <button onClick={props.deleteNode}>Delete </button>
-        <button onClick={props.cutNode}>Cut</button>
-        <button onClick={props.pasteNode}>Paste</button>
+        <button
+          onClick={props.deleteNode}
+          disabled={props.node.id === undefined}
+        >
+          Delete{' '}
+        </button>
+        <button onClick={props.cutNode} disabled={props.node.id === undefined}>
+          Cut
+        </button>
+        <button
+          onClick={props.pasteNode}
+          disabled={props.pasteOption.children === undefined}
+        >
+          Paste
+        </button>
         <button onClick={props.point}>Make Point</button>
       </div>
     </div>
